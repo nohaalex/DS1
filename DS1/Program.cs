712400f1f754c72fbd4ff085c09b5c8cecce7ffb
nohaalex <alexnoha37@gmail.com>
2023-09-22 11:54:48 +0530

    Initial Commit

---
+++ DS1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DS1
+{
+    public class DS1
+    {
+        public static void Main(string[] args)
+        {
+            int total = 0;
+            int i = 1;
+            Console.WriteLine("Enter the number");
+            int n=Convert.ToInt32(Console.ReadLine()); 
+            while (i <=n)
+            {
+                total += i;
+                i++;
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
